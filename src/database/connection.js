3ee1e8ee291f892406c835f09d0a054e0edffb59
Fulano de tal <monteiroeduardo@outlook.com>
2022-05-10 18:34:30 -0300

    ajuste final

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const connection = new Sequelize(
-  'postgres://jogos_user:p4rnDzE3cvpEGpLKfn8G93g0IqjG4Jia@dpg-c9srdq7d17cd03l5g1c0-a.oregon-postgres.render.com/jogos',
+    'postgres://jogos_user:p4rnDzE3cvpEGpLKfn8G93g0IqjG4Jia@dpg-c9srdq7d17cd03l5g1c0-a.oregon-postgres.render.com/jogos',
    {
         dialect: 'postgres',
         dialectOptions: {
